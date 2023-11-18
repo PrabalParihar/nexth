e5ca65da35d9c54b3c25e0e63daf629df106056d
0xPrabal.eth <prabalparihar95@gmail.com>
2023-11-18 19:27:56 +0530

    WEB3 STORAGE

--- packages/app/src/components/Upload.tsx
+++ packages/app/src/components/Upload.tsx
@@ -1,64 +1,59 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const PromptUploader: React.FC = () => {
   const [prompt, setPrompt] = useState<string>('');
   const [uploading, setUploading] = useState<boolean>(false);
+  const [cid, setCid] = useState<string>('');
   const [retrievedData, setRetrievedData] = useState<string>('');
 
   const handlePromptChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(event.target.value);
   };
 
-  const retrieveFromPinata = async (cid: string) => {
-    try {
-      const gatewayUrl = `https://gateway.pinata.cloud/ipfs/${cid}`;
-
-      const response = await axios.get(gatewayUrl);
-      setRetrievedData(JSON.stringify(response.data, null, 2));
-      
-      console.log("Retrieved Data:", response.data); // Logging retrieved data
-    } catch (error:any) {
-      console.error("Error retrieving data:", error); // Logging any errors
-    }
-  };
-
-  const uploadToPinata = async () => {
+  const uploadStringData = async () => {
     setUploading(true);
     try {
-      const pinataUrl = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
-      const pinataApiKey = "7a12d189e152acaa686c";
-      const pinataSecretApiKey = "915973e77af2cf16f1918ac336d09dea2f9217f8a10424613c12b5ff19c063fb";
-  
-      const response = await axios.post(
-        pinataUrl,
-        { prompt },
-        {
-          headers: {
-            pinata_api_key: pinataApiKey,
-            pinata_secret_api_key: pinataSecretApiKey,
-          },
-        }
-      );
-  
-      console.log("Upload Response:", response); // Logging upload response
-      if (response.data && response.data.IpfsHash) {
-        await retrieveFromPinata(response.data.IpfsHash); // Retrieve the data using the obtained CID
-      }
-    } catch (error:any) {
-      console.error("Error:", error); // Enhanced error logging
-      if (error.response) {
-        console.error("Error Response:", error.response.data); // Log response data from server
-      }
+      const url = 'https://api.nft.storage/upload';
+      const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVkRjZjN2MwRjFiZmVCRjU3ZTlBYjQ1MDgxRkM2RTk0ZDRjNzk1MDIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5NTk4Nzc3MTAwNCwibmFtZSI6Im5mdCJ9.LK8knz45WvS__jRf6DIZ5UU5t6c4C1tbBMbrNwY_-Tk";
+
+      const blob = new Blob([prompt], { type: 'text/plain' });
+      const formData = new FormData();
+      formData.append('file', blob, 'data.json');
+
+      const response = await axios.post(url, formData, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log("Upload Response:", response.data);
+      const newCid = response.data.value.cid;
+      setCid(newCid); // Store the CID for later retrieval
+      await retrieveData(newCid); // Automatically retrieve data after upload
+    } catch (error) {
+      console.error("Error:", error);
     } finally {
       setUploading(false);
     }
   };
 
+  const retrieveData = async (currentCid: string) => {
+    try {
+      const gatewayUrl = `https://${currentCid}.ipfs.nftstorage.link/data.json`;
+      const response = await axios.get(gatewayUrl);
+      // Prepend new data above old data
+      setRetrievedData(prevData => `${response.data}\n${prevData}`);
+    } catch (error) {
+      console.error("Error retrieving data:", error);
+    }
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    uploadToPinata();
+    uploadStringData();
   };
 
   return (
@@ -68,7 +63,7 @@ const PromptUploader: React.FC = () => {
           type="text"
           value={prompt}
           onChange={handlePromptChange}
-          placeholder="Enter your prompt"
+          placeholder="Enter your data"
           disabled={uploading}
         />
         <button type="submit" disabled={uploading}>
